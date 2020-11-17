3e3bf5bc8b9a485820456efaafe74680c4dfadd3
Kelvin Lu <kellu1997@gmail.com>
2020-11-17 14:46:41 -0800

    make it easier to code backend

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { showUI, on } from '@create-figma-plugin/utilities'
 
 import { Message } from 'src/types/message';
+import { EventMetadata } from 'src/types/event';
 
 
 export default function () {
   const options = { width: 400, height: 400 }
-  on(Message.ADD_EVENT, (...args) => {
-    console.log(args)
+  on(Message.ADD_EVENT, (event: EventMetadata) => {
+    console.log(event)
   })
   showUI(options, {})
 }
