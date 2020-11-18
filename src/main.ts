c03f083d7725ce4b1fc09feea2c2ec452c4895b3
John Tran <jptran318@gmail.com>
2020-11-18 14:57:10 -0800

    feat: initial createLabel

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,28 @@ import { MOCK_EVENTS } from 'src/assets/mockEvents';
 import { Message } from 'src/types/message';
 import { EventMetadata } from 'src/types/event';
 
+function createLabel(event: EventMetadata): void {
+  const container = figma.createFrame();
+  const rect = figma.createRectangle();
+  const name = figma.createText();
+  const description = figma.createText();
+  const notes = figma.createText();
+  figma.loadFontAsync({ family: 'Roboto', style: 'Regular' }).then(() => {
+    name.insertCharacters(0, event.name);
+    description.insertCharacters(0, event.description);
+    notes.insertCharacters(0, event.notes);
+    container.appendChild(rect);
+    container.appendChild(name);
+    container.appendChild(description);
+    container.appendChild(notes);
+    figma.currentPage.appendChild(container);
+  }).catch(() => console.error('problem loading font'));
+}
+
 export default async function (): Promise<void> {
   on(Message.ADD_EVENT, (event: EventMetadata) => {
     console.log(event);
+    createLabel(event);
   });
 
   on(Message.API_KEY, (apiKey: string) => {
