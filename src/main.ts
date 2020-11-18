dce5f4fd8d47f2a09e065e94dd5f49ea7d2134fc
Kelvin Lu <kellu1997@gmail.com>
2020-11-18 02:13:08 -0800

    get the api and secret keys

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,23 @@ import { showUI, on } from '@create-figma-plugin/utilities';
 import { Message } from 'src/types/message';
 import { EventMetadata } from 'src/types/event';
 
-export default function (): void {
+export default async function (): Promise<void> {
   const options = { width: 400, height: 400 };
   on(Message.ADD_EVENT, (event: EventMetadata) => {
     console.log(event);
   });
 
   on(Message.API_KEY, (apiKey: string) => {
-    console.log(apiKey);
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    figma.clientStorage.setAsync('API_KEY', apiKey);
   });
-  showUI(options, {});
+
+  on(Message.SECRET_KEY, (secretKey: string) => {
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    figma.clientStorage.setAsync('SECRET_KEY', secretKey);
+  });
+
+  const initialApiKey: string = (await figma.clientStorage.getAsync('API_KEY')) as string;
+  const initialSecretKey: string = (await figma.clientStorage.getAsync('SECRET_KEY')) as string;
+  showUI(options, { initialApiKey, initialSecretKey });
 }
