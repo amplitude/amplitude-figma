82286301065c7b59af35c1611fba04b970007f15
Kelvin Lu <kellu1997@gmail.com>
2020-11-18 22:34:39 -0800

    linting

--- src/services/csv.service.ts
+++ src/services/csv.service.ts
@@ -1,16 +1,22 @@
+interface AcceptableData {
+  [key: string]: null | undefined | Date | string | number | boolean;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export class CsvDataService {
-    static exportToCsv(filename: string, rows: object[]) {
-      if (!rows || !rows.length) {
-        return;
-      }
-      const separator = ',';
-      const keys = Object.keys(rows[0]);
-      const csvData =
-        keys.join(separator) +
-        '\n' +
+  static exportToCsv(filename: string, rows: AcceptableData[]): void {
+    if (rows.length === 0) {
+      return;
+    }
+    const separator = ',';
+    const keys = Object.keys(rows[0]);
+    const csvData =
+        `${keys.join(separator)
+        }\n${
         rows.map(row => {
           return keys.map(k => {
-            let cell = row[k] === null || row[k] === undefined ? '' : row[k];
+            let cell = row[k];
+            cell ??= '';
             cell = cell instanceof Date
               ? cell.toLocaleString()
               : cell.toString().replace(/"/g, '""');
@@ -19,24 +25,24 @@ export class CsvDataService {
             }
             return cell;
           }).join(separator);
-        }).join('\n');
-  
-      const blob = new Blob([csvData], { type: 'text/csv;charset=utf-8;' });
-      if (navigator.msSaveBlob) { // IE 10+
-        navigator.msSaveBlob(blob, filename);
-      } else {
-        const link = document.createElement('a');
-        if (link.download !== undefined) {
-          // Browsers that support HTML5 download attribute
-          const url = URL.createObjectURL(blob);
-          link.setAttribute('href', url);
-          link.setAttribute('download', filename);
-          link.style.visibility = 'hidden';
-          document.body.appendChild(link);
-          link.click();
-          document.body.removeChild(link);
-        }
+        }).join('\n')}`;
+
+    const blob = new Blob([csvData], { type: 'text/csv;charset=utf-8;' });
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+    if (navigator.msSaveBlob) { // IE 10+
+      navigator.msSaveBlob(blob, filename);
+    } else {
+      const link = document.createElement('a');
+      if (link.download !== undefined) {
+        // Browsers that support HTML5 download attribute
+        const url = URL.createObjectURL(blob);
+        link.setAttribute('href', url);
+        link.setAttribute('download', filename);
+        link.style.visibility = 'hidden';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
       }
     }
   }
-  
+}
