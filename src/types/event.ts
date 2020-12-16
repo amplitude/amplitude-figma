f7445c72afd2b18d9418d6f8721bc8efca3abcfe
Kelvin Lu <kellu1997@gmail.com>
2020-12-16 01:23:38 -0800

    remove unused constnat

--- src/types/event.ts
+++ src/types/event.ts
@@ -23,18 +23,3 @@ export enum NodeMarker {
 export type PluginData = {
   [key in NodeMarker]: string
 };
-
-export const MOCK_EVENTS: EventMetadata[] = [
-  {
-    name: 'Checkout Clicked',
-    trigger: Trigger.ON_CLICK,
-    description: 'When the checkout button is clicked',
-    notes: 'Should have A/B test information'
-  },
-  {
-    name: 'Add to Cart',
-    trigger: Trigger.ON_CLICK,
-    description: 'Ex: “user clicks on the Add to Cart button”',
-    notes: 'Ex: “user clicks on the Add to Cart button”'
-  },
-];
